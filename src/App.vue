1980914e581d1ae7adbe023277f99ead8e9c1a06
Craig <cg37@outlook.com>
2024-09-09 16:31:58 +0800

    feat: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <TopBar />
+</template>
+
+<script setup lang="ts">
+import TopBar from '@/components/TopBar/index'
+</script>
+
+<style scoped></style>
---
+++ src/components/TopBar/TopBar.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="container">
+  Craig's Note
+</div>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 100px;
+  border-bottom:1px solid;
+  background-color: adjust-color($color: #000000, $red: 1, $green: 2, $blue: 3, $hue: 0, $saturation: 0%, $lightness: 0%, $alpha: 1.0);
+}
+</style>
