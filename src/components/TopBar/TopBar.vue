52dda6f05b035bac552f777fa89cf731516cb8cc
Craig <cg37@outlook.com>
2024-09-12 09:01:00 +0800

    feat: 标题栏测试

--- src/components/TopBar/TopBar.vue
+++ src/components/TopBar/TopBar.vue
@@ -10,7 +10,9 @@
 .container {
   width: 100%;
   height: 100px;
-  border-bottom:1px solid;
-  background-color: adjust-color($color: #000000, $red: 1, $green: 2, $blue: 3, $hue: 0, $saturation: 0%, $lightness: 0%, $alpha: 1.0);
+  position: absolute;
+  top: 0;
+  background-color: white;
+  color: black
 }
 </style>
