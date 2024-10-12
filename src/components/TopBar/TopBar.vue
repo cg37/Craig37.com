63dfabb7773d7c7820f2d2176ae4db92d604e041
Craig <cg37@outlook.com>
2024-10-12 08:01:23 +0800

    feat: 解决了vite配置的引用替代问题

--- src/components/TopBar/TopBar.vue
+++ src/components/TopBar/TopBar.vue
@@ -11,6 +11,6 @@
   width: 100%;
   height: 100px;
   border-bottom:1px solid;
-  background-color: adjust-color($color: #000000, $red: 1, $green: 2, $blue: 3, $hue: 0, $saturation: 0%, $lightness: 0%, $alpha: 1.0);
+  background-color: black;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': pathResolve('./src'),
+      '@': resolve('src')
     }
   }
 })
