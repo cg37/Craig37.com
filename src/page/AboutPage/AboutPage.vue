20affb387c382e1c682e1d42c66adcd5890826b1
Craig <cg37@outlook.com>
2025-05-24 23:03:31 +0800

    feat: 适配路由

--- src/page/AboutPage/AboutPage.vue
+++ src/page/AboutPage/AboutPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="first-paint">
     <TopBar />
+    111
   </div>
-  <div class="content" />
 </template>
 <script setup lang="ts">
 import TopBar from '@/components/TopBar/index';
@@ -11,12 +11,13 @@ import TopBar from '@/components/TopBar/index';
 .first-paint {
   position: absolute;
   width: 100%;
-  height: 100%;
+  height: 80%;
   background-image: url('/src/assets//howl049.jpg');
   background-repeat:no-repeat;
   background-size: cover;
   background-attachment: fixed;
   background-position: center;
+  border: 1px solid red;
 }
 .content {
   color: black;
