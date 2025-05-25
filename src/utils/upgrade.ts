332b82e805a0c23b3bcc43f2ec522b04a860d42d
Craig <cg37@outlook.com>
2025-05-25 22:29:45 +0800

    feat: 添加页面更新自动刷新功能

---
+++ src/utils/upgrade.ts
@@ -0,0 +1,41 @@
+let lastSrcs: string | any[]
+const scriptReg = /<scripts.*?src(["'])(?<src>.*?)\1.*?>/gm
+
+async function extreactNewScripts() {
+  const html = await fetch('?/_timestamp' + Date.now()).then((resp) => resp.text());
+  scriptReg.lastIndex = 0;
+  const res = []
+  let match;
+  while((match = scriptReg.exec(html))) {
+    res.push(match.groups?.src)
+  }
+  return res;
+}
+
+export async function needUpdate() {
+  const newScripts = await extreactNewScripts();
+  if (!lastSrcs) {
+    lastSrcs = newScripts;
+    return false;
+  }
+  let res = false;
+  if (lastSrcs.length !== newScripts.length) {
+    res = true;
+  }
+  for (let i = 0; i < lastSrcs.length; i++) {
+    if (lastSrcs[i] !== newScripts[i]) {
+      res = true;
+      break
+    }
+  }
+  return res
+}
+
+export async function upgrageRefresh() {
+  const willUpdate = await needUpdate();
+  if (willUpdate) {
+    console.log('需要更新');
+    window.location.reload();
+  }
+  return;
+}
