4c5a5690c868ae48eb90e3a89b5ddfaff5fcb1bc
Craig <cg37@outlook.com>
2025-05-26 22:15:34 +0800

    feat: 添加prettier插件

--- eslint.config.js
+++ eslint.config.js
@@ -3,28 +3,30 @@ import globals from "globals";
 import tseslint from "typescript-eslint"; //推荐 ts 规范
 import pluginVue from "eslint-plugin-vue"; //推荐 vue 规范
 import { defineConfig } from "eslint/config";
-
+import prettierRecommnded from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
   {
-		files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
-		plugins: { js },
-		extends: ["js/recommended"]
-	},{
-		files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
-		languageOptions: { globals: {...globals.browser, ...globals.node} }
-	},
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
   {
-		files: ["**/*.vue"],
-		languageOptions: {
-			parserOptions: {
-				parser: tseslint.parser
-			}
-		}
-	},
-	{
-		ignores: ['*.d.ts']
-	}
+    files: ["**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser
+      }
+    }
+  },
+  {
+    ignores: ["*.d.ts"]
+  },
+  prettierRecommnded
 ]);
